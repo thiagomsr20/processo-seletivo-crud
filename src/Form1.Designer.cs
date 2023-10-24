52f672e44603ba4bdf1a852e2ab33e1974887cc8
thiagomsr20 <thiagoannotationdev2017@gmail.com>
2023-10-24 15:36:07 -0300

    Querys e propriedades da janela

--- src/Form1.Designer.cs
+++ src/Form1.Designer.cs
@@ -29,10 +29,23 @@ namespace erpfake
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(700, 400);
+            this.Name = "Form1";
+            this.Text = "Cadastro de Material";
+
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+
+            this.ResumeLayout(false);
+
         }
 
         #endregion
